98cf25ffc51cc7c5664a2fd9fc0e43897f671968
Bill <hb1998@yandex.ru>
2019-10-05 01:05:00 +0300

    A VOT TUT NE ROBIT

--- lab1/main.c
+++ lab1/main.c
@@ -11,15 +11,16 @@ int main(void)
 {
     int count = 0;
     puts("Start main");
-    printf("MAIN - The proccess id in main : %d\n", ++count, getpid());
-    printf("MAIN - The parent proccess id in main : %d\n", count, getppid());
+    printf("MAIN - The proccess id in main : %d\n", getpid());
+    printf("MAIN - The parent proccess id in main : %d\n", getppid());
+       __pid_t pid;
+    printf("new pid : %d\n",pid = fork());
     int res = on_exit(onend_handler,0);
     int res1 = atexit(atend_handler);
     if(res != 0 && res1 != 0) 
         puts("Atexit error\n");
 
-    __pid_t pid;
-    printf("new pid : %d\n",pid = fork());
+    
     waitpid(pid, 0, 0);
     
     return 0;
@@ -27,6 +28,7 @@ int main(void)
 
 void onend_handler(int,void*)
 {
+    int a = 1/0;
     static int count = 0;
     puts("Start onexit");
     printf("%d - The proccess id in on_exit : %d\n", ++count, getpid());
