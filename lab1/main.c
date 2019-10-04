22330ebb2cc6517bb1bbfaac77a69034c12a3e95
Bill <hb1998@yandex.ru>
2019-10-05 00:50:49 +0300

    VRODE ROBIT V2

--- lab1/main.c
+++ lab1/main.c
@@ -4,25 +4,39 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 
-void end_handler(void);
+void onend_handler(int,void*);
+void atend_handler(void);
 
 int main(void)
 {
+    int count = 0;
     puts("Start main");
-
-    int res = atexit(end_handler);
-    if(res != 0)
+    printf("MAIN - The proccess id in main : %d\n", ++count, getpid());
+    printf("MAIN - The parent proccess id in main : %d\n", count, getppid());
+    int res = on_exit(onend_handler,0);
+    int res1 = atexit(atend_handler);
+    if(res != 0 && res1 != 0) 
         puts("Atexit error\n");
 
     __pid_t pid;
     printf("new pid : %d\n",pid = fork());
-
     waitpid(pid, 0, 0);
+    
     return 0;
 }
 
-void end_handler(void)
+void onend_handler(int,void*)
 {
     static int count = 0;
-    printf(" %d - The proccess id in exit : %d\n", ++count, getpid());
+    puts("Start onexit");
+    printf("%d - The proccess id in on_exit : %d\n", ++count, getpid());
+    printf("%d - The parent proccess id in on_exit : %d\n", count, getppid());
+}
+
+void atend_handler(void)
+{
+    static int count = 0;
+    puts("Start atexit");
+    printf("%d - The proccess id in atexit : %d\n", ++count, getpid());
+    printf("%d - The parent proccess id in atexit : %d\n", count, getppid());
 }
