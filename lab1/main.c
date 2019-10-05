ec5de715374eb8392b4cedefc0c22496a0e08fec
Bill <hb1998@yandex.ru>
2019-10-05 09:37:54 +0300

    hook was replaced and added logs in res.txt

--- lab1/main.c
+++ lab1/main.c
@@ -11,24 +11,28 @@ int main(void)
 {
     int count = 0;
     puts("Start main");
+  
+
     printf("MAIN - The proccess id in main : %d\n", getpid());
     printf("MAIN - The parent proccess id in main : %d\n", getppid());
-       __pid_t pid;
-    printf("new pid : %d\n",pid = fork());
+     
+
     int res = on_exit(onend_handler,0);
     int res1 = atexit(atend_handler);
     if(res != 0 && res1 != 0) 
         puts("Atexit error\n");
+      
+        __pid_t pid;
+    printf("new pid : %d\n",pid = fork());
 
-    
     waitpid(pid, 0, 0);
-    
+    exit(1);
     return 0;
 }
 
 void onend_handler(int,void*)
 {
-    int a = 1/0;
+   
     static int count = 0;
     puts("Start onexit");
     printf("%d - The proccess id in on_exit : %d\n", ++count, getpid());
@@ -37,6 +41,7 @@ void onend_handler(int,void*)
 
 void atend_handler(void)
 {
+
     static int count = 0;
     puts("Start atexit");
     printf("%d - The proccess id in atexit : %d\n", ++count, getpid());
